f88cea7d630ccf497c74e21720ef851bf39f77ff
Vadim Pushtaev <pushtaev.vm@gmail.com>
2025-01-20 00:42:03 +0200

    Fail if enable_classmethod_decorators is forgotten

--- src/classmethod_decorator/decorator.py
+++ src/classmethod_decorator/decorator.py
@@ -1,20 +1,36 @@
-from typing import Callable, ParamSpec, TypeVar, Any, Protocol
+from dataclasses import dataclass
+from typing import Callable, ParamSpec, TypeVar, Any, Protocol, Generic, cast
+
+from classmethod_decorator.exceptions import ClassNotDecoratedError
 
 CLASS = TypeVar("CLASS", bound=type)
 P = ParamSpec("P")
-R = TypeVar("R")
+R = TypeVar("R", covariant=True)
 
 
-class _HasNameProtocol(Protocol):
+class _ClassMethod(Protocol):
     __name__: str
 
 
+@dataclass(kw_only=True, frozen=True)
+class _WrappedMethod(Generic[P, R]):
+    wrapped: Callable[P, R]
+    class_method: _ClassMethod
+
+    def __call__(self, *args: P.args, **kwargs: P.kwargs) -> R:
+        raise ClassNotDecoratedError(
+            f"Class method {self.wrapped.__name__} was decorated with @classmethod_decorator, "
+            f"but the class does not support classmethod decorators."
+        )
+
+
 def classmethod_decorator(
-    class_method: _HasNameProtocol,
+    class_method: _ClassMethod,
 ) -> Callable[[Callable[P, R]], Callable[P, R]]:
     def decorator(method: Callable[P, R]) -> Callable[P, R]:
-        setattr(method, "__classmethod_decorator__", class_method)
-        return method
+        # We cannot expose that the actual type is _WrappedMethod,
+        # because it should soon become a method again and must look like a method from the outside.
+        return cast(Callable[P, R], _WrappedMethod(wrapped=method, class_method=class_method))
 
     return decorator
 
@@ -24,11 +40,16 @@ def enable_classmethod_decorators[CLASS](klass: CLASS) -> CLASS:
     attr: Any
 
     for attr_name, attr in klass.__dict__.items():
-        if (class_method := getattr(attr, "__classmethod_decorator__", None)) is not None:
+        if isinstance(attr, _WrappedMethod):
+            class_method = attr.class_method
             # find this class_method in the class
             class_method_to_be_called = getattr(klass, class_method.__name__, None)
             if class_method_to_be_called is None:
                 raise AttributeError(f"Class method {class_method.__name__} not found in class {klass}")
+
+            # We cannot make it part of the interface, so let's require in runtime
+            if not hasattr(class_method, "__func__"):
+                raise AttributeError(f"Class method {class_method.__name__} in class {klass} is not a class method")
 
             # check that we found the correct class method
             if class_method_to_be_called.__func__ is not class_method.__func__:
@@ -37,6 +58,6 @@ def enable_classmethod_decorators[CLASS](klass: CLASS) -> CLASS:
                 )
 
             # decorate
-            setattr(klass, attr_name, class_method_to_be_called(attr))
+            setattr(klass, attr_name, class_method_to_be_called(attr.wrapped))
 
     return klass
--- tests/test_decorator.py
+++ tests/test_decorator.py
@@ -1,6 +1,9 @@
 from typing import Callable, Self
 
+import pytest
+
 from classmethod_decorator.decorator import enable_classmethod_decorators, classmethod_decorator
+from classmethod_decorator.exceptions import ClassNotDecoratedError
 
 
 @enable_classmethod_decorators
@@ -21,7 +24,26 @@ class Dummy:
         return value**2
 
 
+class DummyNotDecorated:
+    @classmethod
+    def double(cls, method: Callable[[Self, int], int]) -> Callable[[Self, int], int]:
+        def decorated(self: Self, value: int) -> int:
+            return 2 * method(self, value)
+
+        return decorated
+
+    @classmethod_decorator(double)
+    def sqr(self, value: int) -> int:
+        return value**2
+
+
 def test_decorator() -> None:
     dummy = Dummy()
     assert dummy.sqr(2) == 4 * 2  # doubled
     assert len(Dummy.DECORATED) == 1
+
+
+def test_forgotten_class_decorator() -> None:
+    dummy = DummyNotDecorated()
+    with pytest.raises(ClassNotDecoratedError):
+        dummy.sqr(2)
